a5beef6c2d3c30e4d57a36f61198f0b77b371393
kcgm <83209104+kcgm@users.noreply.github.com>
2025-11-10 20:49:18 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const webpack = require("webpack");
-require("dotenv").config();
+require('dotenv').config();
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "production",
-  entry: "./rsvp.js",
+  mode: 'production',
+  entry: './rsvp.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'rsvp.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.AIRTABLE_API_KEY": JSON.stringify(process.env.AIRTABLE_API_KEY),
-      "process.env.AIRTABLE_BASE_ID": JSON.stringify(process.env.AIRTABLE_BASE_ID),
+      'process.env.AIRTABLE_API_KEY': JSON.stringify(process.env.AIRTABLE_API_KEY),
+      'process.env.AIRTABLE_BASE_ID': JSON.stringify(process.env.AIRTABLE_BASE_ID),
     }),
   ],
 };
